fb141c7ad5c4b355bb3b523e844f362d7e21fb85
Levis56 <leviskinzy56@gmail.com>
2022-03-13 11:52:12 -0700

    My 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,19 +12,17 @@ int digit1, digit2;
 
 for (digit1 = 0; digit1 < 10; digit1++)
 {
-
+putchar((digit1 % 10) + '0');
+}
 for (digit2 = 0; digit2 < 10; digit2++)
 {
-putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
+}
 
 if (digit1 == 9 && digit2 == 9)
 continue;
 
 putchar(',');
-putchar(',');
-}
-}
 
 putchar('\n');
 
