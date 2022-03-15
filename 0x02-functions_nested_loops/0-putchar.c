dcd891d4a6d2a80da3ae48bb093013ddbadd2beb
Levis56 <leviskinzy56@gmail.com>
2022-03-15 10:43:07 -0700

    My 0-putzhar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * main - Prints "_putchar" followed by a new line.
+ * main - Prints "_putchar"
  *
- * Return: Always 0 (Success/correct)
+ * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
+
 _putchar( '_' );
 _putchar( 'p' );
 _putchar( 'u' );
