ba8ae25ce3b32aca9a67580b220ec75e36b9f3df
Levis56 <leviskinzy56@gmail.com>
2022-03-17 13:46:04 -0700

    My 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,17 @@
  */
 void print_most_numbers(void)
 {
+
 int i;
 
-for (i = 0; i <= 9; i++)
+i = 0;
+while (i < 10)
 {
 if (i != 2 && i != 4)
-_putchar((num % 10) + '0');
+{
+_putchar (i + '0');
+}
+i++;
 }
 
 _putchar('\n');
