b0cd16477f92ea545ec3433bede6559b08ad5100
Levis56 <leviskinzy56@gmail.com>
2022-03-17 12:20:02 -0700

    My 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,16 +2,15 @@
 /**
  * _isdigit - evaluate if is a digit
  * @c: print int
- * Return: ALways 0 (success/correct)
+ * Return: 1 if the number is a digit, else 0.
  */
 
 int _isdigit(int c)
 {
-if (c >= 48 && c <=57)
+if (c >=  '48' && c <= '57')
 {
 return (1);
 }
-
 
 else
 {
