c9d8e3ec29a16a8a2419ff486c9c2f301a08a8c7
Levis56 <leviskinzy56@gmail.com>
2022-03-17 15:17:33 -0700

    My 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,15 +3,15 @@
  * main - Prints numbers from 1-100, but for multiples of 3,
  * 	     Fizz is printed instead of the number while for of 5,
  *	     Buzz  is printed and FizzBuzz is printed for multiples of both 3 and 5.
- *
  * Return: 0 Always is a (Success/Correct).
  */
 int main(void)
 {
+
 int p = 100;
 int a;
 
-a = 100;
+a = 1;
 while (a <= p)
 {
 if (a % 3 == 0 && a % 5 == 0)
