8e8ee7de8e85fb207462920a69df26e109fda086
Levis56 <leviskinzy56@gmail.com>
2022-03-21 11:23:23 -0700

    My 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 
 int is_numerical(unsigned int n)
 {
-return (n >= '0' && n <='9');
+return (n >= '0' && n <= '9');
 }
 
 /**
@@ -31,9 +31,9 @@ for (i = 0; s[i] != '\0'; i++)
 {
 if (is_numerical(s[i]))
 {
-number = (s[i] -48) + number * 10;
+number = (s[i] - 48) + number * 10;
 
-if (s[i + 1] == '-')
+if (s[i + 1] == ' ')
 break;
 }
 else if (s[i] == '-')
